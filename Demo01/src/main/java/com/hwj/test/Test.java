3c6198df94c2b87eb776785699cc1444612fee8e
zhiyuan <zhiyuan@qq.com>
2022-05-08 21:38:25 +0800

    git-maven-01 commit

---
+++ Demo01/src/main/java/com/hwj/test/Test.java
@@ -0,0 +1,11 @@
+package com.hwj.test;
+
+/**
+ * @author shkstart
+ * @create 2022-05-08  21:36
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello gitee!!");
+    }
+}
